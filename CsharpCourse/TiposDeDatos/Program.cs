6ba86c12c4f14466225d552fbdb33d5adbb27c0d
JimcostDev <rjak727@gmail.com>
2022-06-21 18:52:35 +0200

    iniciamos el curso de c#

---
+++ CsharpCourse/TiposDeDatos/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// el tipo de dato define que puedo almacenar en la variable
+int number = 27;
+double doubleNumber = 12.334;
+char character = 'a';
+string name = "Ronaldo";
+bool isOlder = false;
+
+Console.WriteLine("soy un número entero: " + number);
+Console.WriteLine("soy un número flotante: " + doubleNumber);
+Console.WriteLine("soy un caracter: " + character);
+Console.WriteLine("soy una cadena de texto: " + name);
+Console.Write("soy un booleano: " + isOlder);
+
---
+++ CsharpCourse/Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿// variable =  nos sirve para guardar información temporalmente en la memoria del ordenador.
+/*
+    condiciones para crear una variable:
+    1. debe comenzar con letras (a-z) o con _ pero no puede empezar con numeros
+    2. se debe especificar primero el tipo de dato (si es numero, cadena, etc)
+ */
+
+int number1 = 1;
+int number2 = 7;
+
+int sum = number1 + number2;
+
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+Console.WriteLine(sum);
