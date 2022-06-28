148bc80b73ffc28b4eec6f86abf04ab1e74f6f24
JimcostDev <rjak727@gmail.com>
2022-06-28 19:13:00 +0200

    trabajando con funciones

---
+++ CsharpCourse/Funciones/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+// funciones = procesos de codigo que van a repertirse y tu quieras encapsular
+Show();
+Sum(4, 5);
+Sum(7, 7);
+int valor = Mul(2, 2);
+Console.WriteLine("El resultado de la multiplicación es: "+ valor);
+
+
+// void = me indica que no va a retornar información
+
+// funcion sin parametros
+static void Show()
+{
+    Console.WriteLine("Hola soy un texto que se imprime desde una función");
+}
+
+// funcion con parametros
+static void Sum(int a, int b)
+{
+    int sum = a + b;
+    Console.WriteLine(sum);
+}
+
+//funcion que retorna algo (tipo de dato)
+static int Mul(int a, int b)
+{
+    int mul = a * b;
+    return mul;
+}
