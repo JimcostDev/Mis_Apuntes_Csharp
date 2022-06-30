a7417fc98728703236a1a0c16b6664579b1944fc
JimcostDev <rjak727@gmail.com>
2022-06-30 15:52:31 +0200

    sentencia switch

---
+++ CsharpCourse/SentenciaSwitch/Program.cs
@@ -0,0 +1,20 @@
+﻿int value = 1;
+switch (value)
+{
+    case 1:
+        Console.WriteLine("Seleccionaste el 1");
+        break;
+    case 2:
+        Console.WriteLine("Seleccionaste el 2");
+        break;
+    case < 0:
+    case > 100:
+        Console.WriteLine("Fuera de rango");
+        break;
+    case > 4 and < 10:
+        Console.WriteLine("Seleccionaste una opción entre 5 y 9");
+        break ;
+    default:
+        Console.WriteLine("Opción invalida");
+        break;
+}
