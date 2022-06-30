ef68da1999a97ab8af263870cb39bd38dba71f68
JimcostDev <rjak727@gmail.com>
2022-06-30 15:40:25 +0200

    condicional if

---
+++ CsharpCourse/Condicionales/Program.cs
@@ -0,0 +1,29 @@
+﻿bool areYouHungry = true;
+bool areYouHaveMoney = true;
+
+if (areYouHungry && areYouHaveMoney && isOpenRestaurant("Restaurante 24 horas"))
+{
+    Console.WriteLine("Voy a comer afuera, en un restaurante");
+}
+else
+{
+    Console.WriteLine("No comes");
+}
+
+
+static bool isOpenRestaurant(string name, int hour = 0)
+{
+    if(name == "KFC" && hour > 8 && hour < 23)
+    {
+        return true;
+    }
+    else if (name == "Restaurante 24 horas")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
