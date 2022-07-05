9b226971b9c7d52e54caeaee5d3773e29e3140c8
JimcostDev <rjak727@gmail.com>
2022-07-05 18:48:20 +0200

    while y do while

---
+++ CsharpCourse/WhileDoWhile/Program.cs
@@ -0,0 +1,12 @@
+﻿int i = 0;
+while (i < 10) {
+    Console.WriteLine("Iteración de i " + i);
+    i++;
+}
+
+
+bool run = false;
+do
+{
+    Console.WriteLine("Entro una vez y ya");
+} while (run);
