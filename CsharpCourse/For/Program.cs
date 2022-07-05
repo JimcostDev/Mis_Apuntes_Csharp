1f8cff456f4e67bea5dd942dfdb11fbf35ea5cc1
JimcostDev <rjak727@gmail.com>
2022-07-05 18:53:54 +0200

    bucle for

---
+++ CsharpCourse/For/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] friends = new string[7]
+{
+    "Silvia",
+    "Libardo",
+    "Oswa",
+    "Clau",
+    "Tania",
+    "Lucy",
+    "Xiomy"
+};
+
+for (int i = 0; i < friends.Length; i++)
+{
+    Console.WriteLine(friends[i]);
+}
