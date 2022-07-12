2fb81c92a007ac8372b738ead72f962614a2610e
JimcostDev <rjak727@gmail.com>
2022-07-12 18:47:46 +0200

    propiedades en c# - poo

---
+++ CsharpCourse/POO_Propiedades/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+Sale mySale = new Sale(10, DateTime.Now);
+mySale.Total = -120;
+Console.WriteLine(mySale.Total);
+Console.WriteLine(mySale.Date);
+
+
+class Sale
+{
+
+    //propiedades & modificadores de acceso
+    private int total;
+    DateTime date;
+
+    public int Total
+    {
+        // consultar - obtener valor
+        get { return total; } 
+
+        // editar valor
+        set
+        {
+            if (value < 0)
+            {
+                value = 0;
+            }
+            total = value;
+        }
+    }
+
+    public string Date
+    {
+        get { return date.ToLongDateString(); }
+    }
+
+    public Sale(int total, DateTime date)
+    {
+        this.total = total;
+        this.date = date;
+    }
+}
