f3936fd56177f7349b5b83a37f7872b743eedc7f
JimcostDev <rjak727@gmail.com>
2023-04-19 14:54:32 +0200

    json

---
+++ CsharpCourse/Json/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+
+Teams[] teams = new Teams[] {
+    new Teams()
+    {
+        Name =  "Liverpool",
+        Country = "Inglaterra"
+    },
+    new Teams()
+    {
+        Name =  "Arsenal",
+        Country = "Inglaterra"
+    }
+};
+
+// convertir en formato json
+string json = JsonSerializer.Serialize(teams);
+Console.WriteLine(json);
+
+// proceso inverso - deserializar
+Teams[] equipos = JsonSerializer.Deserialize<Teams[]>(json);
+
+public class Teams
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+}
