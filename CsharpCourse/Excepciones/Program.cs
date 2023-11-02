f98fdc9593770e222db33aa44b1f2577c82c9065
JimcostDev <rjak727@gmail.com>
2023-11-02 13:15:57 +0100

    excepciones

---
+++ CsharpCourse/Excepciones/Program.cs
@@ -0,0 +1,25 @@
+﻿
+try
+{
+    string content = File.ReadAllText(@"C:\Users\JIMCOSTDEV\Desktop\hola.txt");
+    Console.WriteLine(content);
+
+    //string content2 = File.ReadAllText(@"C:\Users\JIMCOSTDEV\Desktop\hola2.txt");
+    //Console.WriteLine(content2);
+
+    throw new Exception("Ocurrio algo raro");
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"El archivo no existe");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Aquí me he ejecutado, pase lo que pase, exista o no expeción");
+}
+
+Console.WriteLine("Aqui se sigue ejecutando");
